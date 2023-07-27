e82a49c6c0957068bbd7c40f22d6c3b912566373
iz.zagirov <iz.zagirov@tensor.ru>
2023-07-27 22:14:40 +0300

    date title

--- src/components/CreateDate.js
+++ src/components/CreateDate.js
@@ -3,6 +3,7 @@ import './PriceItem.scss';
 export function CreateDate({date, className}) {
     const creationDate = date.toLocaleDateString('en-GB', {month: 'long', day: 'numeric', year: 'numeric'});
     return <>
+         <h1>Added at</h1>
          <div className={className}>{creationDate}</div>
     </>
 }
