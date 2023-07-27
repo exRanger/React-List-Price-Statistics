57458f09806d1afac529848a68983be217469528
iz.zagirov <iz.zagirov@tensor.ru>
2023-07-27 22:14:40 +0300

    market app re-design

--- src/components/CreateDate.js
+++ src/components/CreateDate.js
@@ -1,8 +1,17 @@
-import './PriceItem.scss';
+import "./PriceItem.scss";
 
-export function CreateDate({date, className}) {
-    const creationDate = date.toLocaleDateString('en-GB', {month: 'long', day: 'numeric', year: 'numeric'});
-    return <>
-         <div className={className}>{creationDate}</div>
-    </>
+const PI = 24;
+
+export function CreateDate({ date, className }) {
+    const creationDate = date.toLocaleDateString("en-GB", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+    });
+    return (
+        <>
+            <h1>Addedss at</h1>
+            <div className={className}>{creationDate}</div>
+        </>
+    );
 }
