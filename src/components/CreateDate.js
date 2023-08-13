47b4683da0a149b11a3c0ed932b496da55afbedd
Zagirov Il'giz <lmnasxzz@gmail.com>
2023-08-13 22:49:15 +0300

    Update CreateDate.js

--- src/components/CreateDate.js
+++ src/components/CreateDate.js
@@ -1,7 +1,5 @@
 import "./PriceItem.scss";
 
-<<<<<<< HEAD
-const PI = 24;
 
 export function CreateDate({ date, className }) {
     const creationDate = date.toLocaleDateString("en-GB", {
@@ -16,12 +14,3 @@ export function CreateDate({ date, className }) {
         </>
     );
 }
-=======
-export function CreateDate({date, className}) {
-    const creationDate = date.toLocaleDateString('en-GB', {month: 'long', day: 'numeric', year: 'numeric'});
-    return <>
-         <h1>Added at</h1>
-         <div className={className}>{creationDate}</div>
-    </>
-}
->>>>>>> e82a49c6c0957068bbd7c40f22d6c3b912566373
