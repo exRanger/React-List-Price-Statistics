01269cf9257dafbf03c4a556bb64a86f43dd83b2
iz.zagirov <iz.zagirov@tensor.ru>
2023-09-06 10:31:30 +0300

    solved the problem with routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import ReactDOM from 'react-dom/client';
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
 import App from './App';
 
 import './index.scss';
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App/>
-);
+root.render( <App/>);
