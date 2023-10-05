d2ba0652b5998a6c77da9b34104bb47989bf171e
Zagirov Il'giz <lmnasxzz@gmail.com>
2023-10-05 17:11:39 +0300

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ContentLoader from './ContentLoader'
+import React from 'react';
+import ContentLoader from './ContentLoader';
 
 export default function Loader({children, countOfLoad}) {
   const contentLoaderItems = new Array(countOfLoad).fill(<ContentLoader/>);
