13b5782ea8a625c9759bf767a09faa28bfa54352
Zagirov Il'giz <lmnasxzz@gmail.com>
2023-12-01 11:51:58 +0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,9 +6,7 @@ import {Link} from "react-router-dom";
  * @return <Header price={price} />
  * @public
  */
-export default function Header({
-    price = 0
-}) {
+export default function Header({price = 0}) {
     return (
         <>
             <div className="header">
