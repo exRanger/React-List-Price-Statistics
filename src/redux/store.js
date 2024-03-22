620547ee14a28f8c0e427f3e5c664b8f33053a2b
Zagirov Il'giz <lmnasxzz@gmail.com>
2024-03-22 22:26:04 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
 import reducer from './slices/filterSlice';
 
 export const store = configureStore({
